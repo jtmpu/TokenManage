b9a3324644fe89c4570bd1168b924c86d6331e76
user <user@DESKTOP>
2019-12-09 22:34:54 +0100

    Added easier ability to disable privilege via PS-dll.

--- TokenManage/PS.cs
+++ TokenManage/PS.cs
@@ -18,16 +18,31 @@ namespace TokenManage
             return String.Format(@"{0}\{1}", user.Domain, user.Username);
         }
 
+        #region Privileges
+
         public static void EnablePrivilege(string privilege)
+        {
+            SetPrivilege(privilege, true);
+        }
+
+        public static void DisablePrivilege(string privilege)
+        {
+            SetPrivilege(privilege, false);
+        }
+
+        public static void SetPrivilege(string privilege, bool enabled)
         {
             var hProc = TMProcessHandle.GetCurrentProcessHandle();
             var hToken = AccessTokenHandle.FromProcessHandle(hProc);
 
             var newPrivs = new List<ATPrivilege>();
-            newPrivs.Add(ATPrivilege.FromValues(privilege, Constants.SE_PRIVILEGE_ENABLED));
+            var attributes = (uint)(enabled ? Constants.SE_PRIVILEGE_ENABLED : Constants.SE_PRIVILEGE_DISABLED);
+            newPrivs.Add(ATPrivilege.FromValues(privilege, attributes));
 
             AccessTokenPrivileges.AdjustTokenPrivileges(hToken, newPrivs);
         }
+
+        #endregion
 
         public static void ListProcesses()
         {
